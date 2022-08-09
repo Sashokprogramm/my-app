dcc80d47c11d1eb72a9b17de68765560450054cf
Alex-Kant <alex18popov@gmail.com>
2022-08-09 18:26:39 +0600

    feat:homework

---
+++ src/components/IntroductionMenu.js
@@ -0,0 +1,19 @@
+import React from "react";
+import About from "./About";
+import Posts from "./Posts";
+import Tags from "./Tags";
+
+const IntroductionMenu = () => {
+    return (
+        <>
+            <div className="w3-col l4">
+                <About/>
+                <Posts/>
+                <Tags/>
+            </div>
+        </>
+
+    )
+
+}
+export default IntroductionMenu;
